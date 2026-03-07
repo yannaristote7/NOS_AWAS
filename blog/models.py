613c04adcb03439e4a559faf44b3b2ce89881e1d
yannaristote7 <yannaristote7@gmail.com>
2026-03-07 15:04:21 +0100

    Ajout des medias

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     def __str__(self):
         return self.title
+    
